a25cd2aa0f2dfaca877e6264bb844bf57221f392
Abhisek <abhisekmaiti19@gmail.com>
2023-08-25 16:20:21 +0530

    fixing update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,12 @@
-import { Center, Heading, Text } from "@chakra-ui/react"
-import { useContext } from "react"
-import GlobalContext from "../Store/GlobalContext"
+import { Center} from "@chakra-ui/react"
+// import { useContext } from "react"
+// import GlobalContext from "../Store/GlobalContext"
 
 const Home = () => {
   return (
     <>
         <Center flexDirection={'column'} gap={5} w={'100%'} pt={5}>
-            <Heading>
-                Home Component Toiri nei
-            </Heading>
-            <Text>Login Tep</Text>
-            <Text>Okhane Signup tep</Text>
-            <Text></Text>
+            
         </Center>
     </>
   )
